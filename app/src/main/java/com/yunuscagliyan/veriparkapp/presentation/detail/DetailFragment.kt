fd30d6e94f96429892cd81f8094a6a55c447d33f
yunuscagliyan <Sog51410>
2021-12-25 18:47:05 +0300

    detail line chart created.

--- app/src/main/java/com/yunuscagliyan/veriparkapp/presentation/detail/DetailFragment.kt
+++ app/src/main/java/com/yunuscagliyan/veriparkapp/presentation/detail/DetailFragment.kt
@@ -1,5 +1,6 @@
 package com.yunuscagliyan.veriparkapp.presentation.detail
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,6 +10,11 @@ import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.yunuscagliyan.veriparkapp.R
 import com.yunuscagliyan.veriparkapp.common.Resource
 import com.yunuscagliyan.veriparkapp.data.remote.model.response.detail.StockDetailModel
@@ -18,15 +24,20 @@ import com.yunuscagliyan.veriparkapp.presentation.MainViewModel
 import com.yunuscagliyan.veriparkapp.presentation.detail.view_model.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.text.DecimalFormat
+import com.github.mikephil.charting.components.LimitLine.LimitLabelPosition
+
+import com.github.mikephil.charting.components.LimitLine
+import com.github.mikephil.charting.components.MarkerView
+
 
 @AndroidEntryPoint
 class DetailFragment : Fragment() {
 
 
-    private var binding:FragmentDetailBinding?=null
+    private var binding: FragmentDetailBinding? = null
 
-    private val viewModel:DetailViewModel by viewModels()
-    private val mainViewModel:MainViewModel by activityViewModels()
+    private val viewModel: DetailViewModel by viewModels()
+    private val mainViewModel: MainViewModel by activityViewModels()
 
 
     override fun onCreateView(
@@ -34,7 +45,7 @@ class DetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding= FragmentDetailBinding.inflate(layoutInflater,container,false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
         return binding?.root
     }
 
@@ -50,65 +61,89 @@ class DetailFragment : Fragment() {
             (activity as? MainActivity)?.setUpToolbar(toolBar)
         }
     }
+
     private fun listenObserver() {
-        viewModel.stockDetail.observe(viewLifecycleOwner){resource->
-            when(resource){
-                is Resource.Error ->{
+        viewModel.stockDetail.observe(viewLifecycleOwner) { resource ->
+            when (resource) {
+                is Resource.Error -> {
                     binding?.apply {
-                        layoutSuccess.visibility=View.GONE
-                        layoutLoading.container.visibility=View.GONE
-                        layoutError.container.visibility=View.VISIBLE
-                        layoutError.tvError.text=resource.message?:""
+                        layoutSuccess.visibility = View.GONE
+                        layoutLoading.container.visibility = View.GONE
+                        layoutError.container.visibility = View.VISIBLE
+                        layoutError.tvError.text = resource.message ?: ""
                     }
                 }
-                is Resource.Loading ->{
+                is Resource.Loading -> {
                     binding?.apply {
-                        layoutSuccess.visibility=View.GONE
-                        layoutLoading.container.visibility=View.VISIBLE
-                        layoutError.container.visibility=View.GONE
+                        layoutSuccess.visibility = View.GONE
+                        layoutLoading.container.visibility = View.VISIBLE
+                        layoutError.container.visibility = View.GONE
                     }
                 }
-                is Resource.Success ->{
+                is Resource.Success -> {
                     resource.data?.let {
                         bindDetail(it)
                     }
                     binding?.apply {
-                        layoutLoading.container.visibility=View.GONE
-                        layoutError.container.visibility=View.GONE
-                        layoutSuccess.visibility=View.VISIBLE
+                        layoutLoading.container.visibility = View.GONE
+                        layoutError.container.visibility = View.GONE
+                        layoutSuccess.visibility = View.VISIBLE
                     }
                 }
             }
         }
     }
+
     private fun bindDetail(detail: StockDetailModel) {
-        val decimalFormat=DecimalFormat("#.##")
+        val decimalFormat = DecimalFormat("#.##")
 
         binding?.apply {
             layoutFirst.apply {
-                tvLeft.text=getString(R.string.detail_page_symbol,mainViewModel.getDecryptedText(detail.symbol?:""))
-                tvRight.text=getString(R.string.detail_page_daily_lowest, decimalFormat.format(detail.lowest?:0))
+                tvLeft.text = getString(
+                    R.string.detail_page_symbol,
+                    mainViewModel.getDecryptedText(detail.symbol ?: "")
+                )
+                tvRight.text = getString(
+                    R.string.detail_page_daily_lowest,
+                    decimalFormat.format(detail.lowest ?: 0)
+                )
             }
             layoutSecond.apply {
-                tvLeft.text=getString(R.string.detail_page_price,decimalFormat.format(detail.price?:0))
-                tvRight.text=getString(R.string.detail_page_daily_highest,decimalFormat.format(detail.highest?:0))
+                tvLeft.text =
+                    getString(R.string.detail_page_price, decimalFormat.format(detail.price ?: 0))
+                tvRight.text = getString(
+                    R.string.detail_page_daily_highest,
+                    decimalFormat.format(detail.highest ?: 0)
+                )
             }
             layoutThird.apply {
-                tvLeft.text=getString(R.string.detail_page_difference,decimalFormat.format(detail.difference?:0))
-                tvRight.text=getString(R.string.detail_page_count,detail.count?:0)
+                tvLeft.text = getString(
+                    R.string.detail_page_difference,
+                    decimalFormat.format(detail.difference ?: 0)
+                )
+                tvRight.text = getString(R.string.detail_page_count, detail.count ?: 0)
             }
             layoutFourth.apply {
-                tvLeft.text=getString(R.string.detail_page_volume,decimalFormat.format(detail.volume?:0))
-                tvRight.text=getString(R.string.detail_page_minimum,decimalFormat.format(detail?.maximum?:0))
+                tvLeft.text =
+                    getString(R.string.detail_page_volume, decimalFormat.format(detail.volume ?: 0))
+                tvRight.text = getString(
+                    R.string.detail_page_minimum,
+                    decimalFormat.format(detail.maximum ?: 0)
+                )
             }
             layoutFifth.apply {
-                tvLeft.text=getString(R.string.detail_page_bid,decimalFormat.format(detail.bid?:0))
-                tvRight.text=getString(R.string.detail_page_maximum,decimalFormat.format(detail.minimum?:0))
+                tvLeft.text =
+                    getString(R.string.detail_page_bid, decimalFormat.format(detail.bid ?: 0))
+                tvRight.text = getString(
+                    R.string.detail_page_maximum,
+                    decimalFormat.format(detail.minimum ?: 0)
+                )
             }
             layoutSixth.apply {
-                tvLeft.text=getString(R.string.detail_page_offer,decimalFormat.format(detail.offer?:0))
-                tvRight.text=getString(R.string.detail_page_change)
-                ivChanging.visibility=View.VISIBLE
+                tvLeft.text =
+                    getString(R.string.detail_page_offer, decimalFormat.format(detail.offer ?: 0))
+                tvRight.text = getString(R.string.detail_page_change)
+                ivChanging.visibility = View.VISIBLE
                 if (detail.isUp == true) {
                     ivChanging.setImageResource(R.drawable.ic_arrow_up)
                     DrawableCompat.setTint(
@@ -123,6 +158,65 @@ class DetailFragment : Fragment() {
                         ContextCompat.getColor(requireContext(), R.color.red_500)
                     )
                 }
+            }
+
+
+            dailyDataChart.apply {
+                setTouchEnabled(true)
+                isDragEnabled = true
+                setScaleEnabled(false)
+                setPinchZoom(false)
+
+                setDrawMarkers(true)
+
+                axisLeft.isEnabled = true
+                xAxis.isEnabled = true
+                xAxis.position = XAxis.XAxisPosition.BOTTOM
+
+                axisRight.isEnabled = false
+                legend.isEnabled = false
+                description.isEnabled = false
+
+
+                val limitLine = LimitLine(detail.maximum?.toFloat()?:1f, "Upper Limit")
+                limitLine.lineWidth = 4f
+                limitLine.enableDashedLine(10f, 10f, 0f)
+                limitLine.labelPosition = LimitLabelPosition.RIGHT_TOP
+                limitLine.textSize = 10f
+
+                axisLeft.addLimitLine(limitLine)
+
+                val entries = ArrayList<Entry>()
+                detail.graphicData?.let {
+                    it.forEach { daily ->
+                        entries.add(
+                            Entry(daily?.day?.toFloat() ?: 0f, daily?.value?.toFloat() ?: 0f)
+                        )
+                    }
+                }
+                val dataSet = LineDataSet(entries, "Daily")
+                dataSet.fillAlpha = 60
+                dataSet.fillColor = Color.RED
+
+
+                dataSet.circleColors =
+                    mutableListOf(ContextCompat.getColor(requireContext(), R.color.black))
+                dataSet.circleHoleColor=Color.BLACK
+                dataSet.enableDashedLine(10f,10f,10f)
+                dataSet.color = Color.BLACK
+                dataSet.valueTextColor = Color.GRAY
+                dataSet.highLightColor = Color.RED
+                dataSet.setDrawValues(true)
+                dataSet.setDrawFilled(true)
+
+                dataSet.lineWidth = 1.0f
+                dataSet.isHighlightEnabled = true
+                dataSet.setDrawHighlightIndicators(true)
+
+                dataSet.axisDependency = YAxis.AxisDependency.LEFT
+
+                val lineData = LineData(dataSet)
+                data = lineData
             }
         }
     }
